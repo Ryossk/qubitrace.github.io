ab4b876ef73075d24b738c355e227143a5e99ac9
Ryossk <15702778+Ryossk@users.noreply.github.com>
2024-04-23 00:37:27 +0900

    two qubit gate

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
     var data = [
-        { "qubit": "superconductor", "T1": 30, "Reference": "[1]",  "one qubit gate": "-"},
-        { "qubit": "superconductor", "T1": 25, "Reference": "[2]",  "one qubit gate": "-"},
-        { "qubit": "photon", "T1": 35, "Reference": "[3]",  "one qubit gate": "-"},
-        { "qubit": "semiconductor", "T1": "-", "Reference": "[3]",  "one qubit gate": "-"}
+        { "qubit": "superconductor", "T1": 30, "Reference": "[1]",  "one qubit gate": "-",  "two qubit gate": "-"},
+        { "qubit": "superconductor", "T1": 25, "Reference": "[2]",  "one qubit gate": "-",  "two qubit gate": "-"},
+        { "qubit": "photon", "T1": 35, "Reference": "[3]",  "one qubit gate": "-",  "two qubit gate": "-" },
+        { "qubit": "semiconductor", "T1": "-", "Reference": "[3]",  "one qubit gate": "-",  "two qubit gate": "-" }
         // Add more objects as needed
     ];
 
@@ -13,6 +13,7 @@ $(document).ready(function() {
             { data: 'qubit' },
             { data: 'T1' },
             { data: 'one qubit gate' },
+            { data: 'two qubit gate' },
             { data: 'Reference' }
         ]
     });
