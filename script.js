70cbe7f37857216daea156b089c70d1981137065
Ryossk <15702778+Ryossk@users.noreply.github.com>
2024-04-23 00:31:22 +0900

    style modified

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
     var data = [
-        { "qubit": "superconductor", "T1": 30, "Reference": "[1]" },
-        { "qubit": "superconductor", "T1": 25, "Reference": "[2]" },
-        { "qubit": "photon", "T1": 35, "Reference": "[3]" }
+        { "qubit": "superconductor", "T1": 30, "Reference": "[1]",  "one qubit gate": "-"},
+        { "qubit": "superconductor", "T1": 25, "Reference": "[2]",  "one qubit gate": "-"},
+        { "qubit": "photon", "T1": 35, "Reference": "[3]",  "one qubit gate": "-"},
+        { "qubit": "semiconductor", "T1": "-", "Reference": "[3]",  "one qubit gate": "-"}
         // Add more objects as needed
     ];
 
@@ -11,6 +12,7 @@ $(document).ready(function() {
         columns: [
             { data: 'qubit' },
             { data: 'T1' },
+            { data: 'one qubit gate' },
             { data: 'Reference' }
         ]
     });
