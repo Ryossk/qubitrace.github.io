9477b0e58aa7bdd5447adb4f1da386bbcb0abcfb
Ryossk <15702778+Ryossk@users.noreply.github.com>
2024-04-23 00:16:08 +0900

    remove JSON

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 $(document).ready(function() {
-    $.getJSON('./data.json', function(data) {
-        // JSONデータを取得してテーブルに表示する
-        var table = $('#myTable').DataTable({
-            data: data,
-            columns: [
-                { data: 'qubit' },
-                { data: 'T1' },
-                { data: 'Reference' }
-            ]
-        });
+    var data = [
+        { "qubit": "superconductor", "T1": 30, "Reference": "[1]" },
+        { "qubit": "superconductor", "T1": 25, "Reference": "[2]" },
+        { "qubit": "photon", "T1": 35, "Reference": "[3]" }
+        // Add more objects as needed
+    ];
+
+    var table = $('#myTable').DataTable({
+        data: data,
+        columns: [
+            { data: 'qubit' },
+            { data: 'T1' },
+            { data: 'Reference' }
+        ]
     });
 });
