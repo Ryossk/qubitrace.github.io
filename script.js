1e48433c4745fd21e2acdd7b04989a644f29e06d
Ryossk <15702778+Ryossk@users.noreply.github.com>
2024-04-22 23:13:56 +0900

    JSON test

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
-    $('#myTable').DataTable();
+    // JSONファイルからデータを読み込む
+    $.getJSON('data.json', function(data) {
+        var table = $('#myTable').DataTable({
+            data: data, // JSONデータを指定
+            columns: [
+                { data: 'name' }, // nameフィールドのデータを表示
+                { data: 'age' }, // ageフィールドのデータを表示
+                { data: 'country' } // countryフィールドのデータを表示
+            ]
+        });
+    });
 });
