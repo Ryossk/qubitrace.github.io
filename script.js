9c4fba6e695682616b1c4bd7f4933e5f45b42775
Ryossk <15702778+Ryossk@users.noreply.github.com>
2024-04-22 23:45:01 +0900

    initialize datatabel after import

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 $(document).ready(function() {
-    // JSONファイルからデータを読み込む
     $.getJSON('./data.json', function(data) {
-        var table = $('#myTable').DataTable({
-            data: data, // JSONデータを指定
-            columns: [
-                { data: 'qubit' }, // qubitフィールドのデータを表示
-                { data: 'T1' }, // T1フィールドのデータを表示
-                { data: 'Reference' } // Referenceフィールドのデータを表示
-            ]
-        });
+        // JSONデータの取得が完了したら、テーブルを初期化する
+        initializeDataTable(data);
     });
 });
+
+function initializeDataTable(data) {
+    var table = $('#myTable').DataTable({
+        data: data,
+        columns: [
+            { data: 'qubit' },
+            { data: 'T1' },
+            { data: 'Reference' }
+        ]
+    });
+}
