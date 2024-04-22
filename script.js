f046c856e4d6fd0cd4eb7021d087b4dafbdf6d00
Ryossk <15702778+Ryossk@users.noreply.github.com>
2024-04-23 01:28:29 +0900

    header style

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
     var data = [
-        { "qubit": "superconductor", "T1": 30, "Reference": "[1]",  "one qubit gate": "-",  "two qubit gate": "-"},
-        { "qubit": "superconductor", "T1": 25, "Reference": "[2]",  "one qubit gate": "-",  "two qubit gate": "-"},
-        { "qubit": "photon", "T1": 35, "Reference": "[3]",  "one qubit gate": "-",  "two qubit gate": "-" },
-        { "qubit": "semiconductor", "T1": "-", "Reference": "[3]",  "one qubit gate": "-",  "two qubit gate": "-" }
+        { "qubit": "superconductor", "T1": 30, "Reference": "Nature",  "one qubit gate": "-",  "two qubit gate": "-"},
+        { "qubit": "superconductor", "T1": 25, "Reference": "Science",  "one qubit gate": "-",  "two qubit gate": "-"},
+        { "qubit": "photon", "T1": 35, "Reference": "arXiv",  "one qubit gate": "-",  "two qubit gate": "-" },
+        { "qubit": "semiconductor", "T1": "-", "Reference": "PRL",  "one qubit gate": "-",  "two qubit gate": "-" }
         // Add more objects as needed
     ];
 
