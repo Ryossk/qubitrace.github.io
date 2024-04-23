7d3630fe29ae226a92076def6d184c3f433fecd1
Ryossk <15702778+Ryossk@users.noreply.github.com>
2024-04-23 09:13:21 +0900

    add column and  spreadseat link

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
 $(document).ready(function() {
     var data = [
-        { "qubit": "superconductor", "T1": 30, "Reference": "Nature",  "one qubit gate": "-",  "two qubit gate": "-"},
-        { "qubit": "superconductor", "T1": 25, "Reference": "Science",  "one qubit gate": "-",  "two qubit gate": "-"},
-        { "qubit": "photon", "T1": 35, "Reference": "arXiv",  "one qubit gate": "-",  "two qubit gate": "-" },
-        { "qubit": "semiconductor", "T1": "-", "Reference": "PRL",  "one qubit gate": "-",  "two qubit gate": "-" }
+        { "qubit": "superconductor", "T1": 30, "Reference": "Nature",  "one_qubit_gate": 1,  "two_qubit_gate": "-", "Year" : "-", "One logical qubit generation Rate": "-", "Comment": "-"},
+        { "qubit": "superconductor", "T1": 25, "Reference": "Science",  "one_qubit_gate": 2,  "two_qubit_gate": "-", "Year" : "-", "One logical qubit generation Rate": "-", "Comment": "-"},
+        { "qubit": "photon", "T1": 35, "Reference": "arXiv",  "one_qubit_gate": 3,  "two_qubit_gate": "-", "Year" : "-", "One logical qubit generation Rate": "-", "Comment": "-" },
+        { "qubit": "semiconductor", "T1": 3, "Reference": "PRL",  "one_qubit_gate": 4,  "two_qubit_gate": "-", "Year" : "-", "One logical qubit generation Rate": "-", "Comment": "-" },
+        { "qubit": "ion", "T1": 100, "Reference": "my note",  "one_qubit_gate": 1,  "two_qubit_gate": 2, "Year" : "-", "One logical qubit generation Rate": "-", "Comment": "-" }
         // Add more objects as needed
     ];
 
-    var table = $('#myTable').DataTable({
+    var table = $('#mainTable').DataTable({
         data: data,
         columns: [
             { data: 'qubit' },
             { data: 'T1' },
-            { data: 'one qubit gate' },
-            { data: 'two qubit gate' },
-            { data: 'Reference' }
+            { data: 'one_qubit_gate' },
+            { data: 'two_qubit_gate' },
+            { data: 'One logical qubit generation Rate' },
+            { data: 'Year' },
+            { data: 'Reference' },
+            { data: 'Comment' }
+
         ]
     });
 });
