f6ac697b54e7a0fa0aca631ae096ed65ab746983
Ryossk <15702778+Ryossk@users.noreply.github.com>
2024-04-24 09:19:39 +0900

    add render for null table

--- script.js
+++ script.js
@@ -5,12 +5,21 @@ $(document).ready(function() {
             columns: [
                 { data: 'qubit' },
                 { data: 'T1' },
+                { data: 'T2*'}
                 { data: 'one_qubit_gate' },
                 { data: 'two_qubit_gate' },
                 { data: 'One logical qubit generation Rate' },
                 { data: 'Year' },
                 { data: 'Reference' },
                 { data: 'Comment' }
+            ],
+            columnDefs: [
+                {
+                    targets: '_all',
+                    render: function(data, type, row) {
+                        return data || "-";
+                    }
+                }
             ]
         });
     });
